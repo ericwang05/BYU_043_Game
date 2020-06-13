e33a7665e99223964fb6722cad7756ad12a2a308
ericwang05 <45795682+ericwang05@users.noreply.github.com>
2020-06-12 19:16:34 -0700

    Fixed player class
    fixed player

--- TicTacToePlayer.py
+++ TicTacToePlayer.py
@@ -1,3 +1,22 @@
+# import random
+# class Player:
+#     def inputPlayerLetter(self):
+#         # Lets the player type which letter they want to be.
+#         # Returns a list with the player's letter as the first item, and the computer's letter as the second.
+#         letter = ''
+#         while not (letter == 'X' or letter == 'O'):
+#             print('Do you want to be X or O?')
+#             letter = input().upper()
+
+#         # the first element in the tuple is the player's letter, the second is the computer's letter.
+#         if letter == 'X':
+#             return ['X', 'O']
+#         else:
+#             return ['O', 'X']
+
+#     def makeMove(self):
+#         board[move] = letter
+
 import random
 class Player:
     def inputPlayerLetter(self):
@@ -16,3 +35,4 @@ class Player:
 
     def makeMove(self):
         board[move] = letter
+
